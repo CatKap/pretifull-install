d6f1f3470fbd280df2b0fbe3e4905d9fb253c2cc
Ilya Dutov <idutov@lachestry.com>
2024-08-12 17:12:03 +0300

    cofirmation

--- install.sh
+++ install.sh
@@ -1,10 +1,30 @@
 #!/bin/sh
 
-# Install kitty 
-#
-#
-#
 
-# Install nvim
-#
+echo 
+
+
+
+# Install kitty 
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin launch=n
+
+
+git clone --depth 1 https://github.com/dexpota/kitty-themes.git ~/.config/kitty/kitty-themes
+
+
+confirm() {
+    read -r -p "${1:  Вы точно Да? [y/N]} " response
+    case "$response" in
+        [yY][eE][sS]|[yY]|[дД]) 
+            true
+            ;;
+        *)
+            false
+            ;;
+    esac
+}
+
+
+
+
 
