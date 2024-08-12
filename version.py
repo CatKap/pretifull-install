d9d7397561b9ee98065fac9dabb9b6ac3d81c809
Ilya Dutov <idutov@lachestry.com>
2024-08-12 16:56:47 +0300

    pySmth

---
+++ version.py
@@ -0,0 +1,11 @@
+#!/bin/env python3
+import requests as R 
+import re
+
+r = R.get("https://github.com/neovim/neovim/releases/latest")
+#print(r.text)
+m = re.search(r"Release NVIM \d+\.\d+.\d+", r.text)
+print(m[0][13:])
+
+
+
