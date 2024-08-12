f626ed4e6b351d4094b7a7daff7c832c94170a08
Ilya Dutov <idutov@lachestry.com>
2024-08-12 18:06:12 +0300

    Kitty-install

--- install.sh
+++ install.sh
@@ -1,17 +1,6 @@
 #!/bin/sh
 
 
-echo 
-
-
-
-# Install kitty 
-curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin launch=n
-
-
-git clone --depth 1 https://github.com/dexpota/kitty-themes.git ~/.config/kitty/kitty-themes
-
-
 confirm() {
     read -r -p "${1:  Вы точно Да? [y/N]} " response
     case "$response" in
@@ -23,6 +12,19 @@ confirm() {
             ;;
     esac
 }
+
+
+
+# Install kitty 
+if ! kitty; then
+  echo "Установка kitty >^ ^<"
+  curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin launch=n
+  
+fi
+
+echo "Установка тем для kitty >>^ ^<<"
+git clone --depth 1 https://github.com/dexpota/kitty-themes.git ~/.config/kitty/kitty-themes
+
 
 
 
